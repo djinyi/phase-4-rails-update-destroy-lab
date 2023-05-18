beaf30bda828a0b4b3f7d78c3d780270e3817b5f
Daisy Yi <daisyyi@rocketmail.com>
2023-05-18 15:35:17 -0700

    commit tree 8522b6a3686227eb770bd35b1ecb1e3171f1254d

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,4 +1,5 @@
 class PlantsController < ApplicationController
+rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
   # GET /plants
   def index
@@ -18,7 +19,28 @@ class PlantsController < ApplicationController
     render json: plant, status: :created
   end
 
+  def update
+    plant = find_plant
+    plant.update(plant_params)
+    render json: plant
+  end
+
+  def destroy
+    plant = find_plant
+    plant.destroy
+    head :no_content
+  end
+
+
   private
+
+  def find_plant
+    Plant.find_by(params[:id])
+  end
+
+  def render_not_found_response
+    render json: { error: "Bird not found" }, status: :render_not_found_response
+  end
 
   def plant_params
     params.permit(:name, :image, :price, :is_in_stock)
